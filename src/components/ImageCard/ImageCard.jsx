ce55182f9a19b18c57e9ba8cd097867015c9d35c
Yuliia Fomenko <yaulia3@gmail.com>
2025-04-25 15:48:23 +0100

    ImageCard fixed

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -3,11 +3,12 @@ import s from "./ImageCard.module.css";
 
 const ImageCard = ({ image, onClick }) => {
   return (
-    <div className={s.card} onClick={onClick}>
+    <div className={s.card}>
       <img
         src={image.urls.small}
         alt={image.alt_description}
         className={s.image}
+        onClick={onClick}
       />
     </div>
   );
